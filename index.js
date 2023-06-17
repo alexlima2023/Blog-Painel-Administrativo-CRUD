98a8b78c00946962bbddc8971ab3bfa16df1a01c
alexlima2023 <alexlima1997@gmail.com>
2023-06-17 14:06:02 -0300

    starting ejs, bodyparser and creating public folder

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Bem vindo ao site!");
+    res.render('index');
 });
+
+//Static -> Carregar arquivos de imgs, css...
+app.use(express.static('public'));
+
+//View Engine -> Configurar para que o express aceite o ejs
+app.set('view engine', 'ejs');
+
+//Body parser -> Configurar para que o express entenda o bodyParser e possa trabalhar com formulários.
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.listen(8080, () => {
     console.log('Servidor está rodando!')
