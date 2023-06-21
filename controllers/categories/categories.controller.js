437a95127def9cdc36d53e05295c5c952d03d620
alexlima2023 <alexlimadev1997@gmail.com>
2023-06-20 21:40:38 -0300

    created the create category route, and its html

--- controllers/categories/categories.controller.js
+++ controllers/categories/categories.controller.js
@@ -1,12 +1,9 @@
 import express from "express";
 const router =  express.Router();
 
-router.get('/categories', (req, res) => {
-    res.send("Rota de categorias"); 
+router.get('/admin/categories/new', (req, res) => {
+    res.render('admin/categories/new');
 });
 
-router.get('/admin/categories/new', (req, res) => {
-    res.send("Rota para criar uma nova categoria"); 
-});
 
 export default router;
--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-const connection = new Sequelize('blog_crud', 'root', '1q2w!Q@W', {
+const connection = new Sequelize('blog_crud', 'alex', '1q2w!Q@W', {
     host: 'localhost',
     dialect: 'mysql'
 });
