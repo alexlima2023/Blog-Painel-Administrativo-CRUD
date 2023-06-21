c606f21abf56471c9946288753dbf1443b7f6c32
alexlima2023 <alexlimadev1997@gmail.com>
2023-06-21 11:28:09 -0300

    create delete categories

--- controllers/categories/categories.controller.js
+++ controllers/categories/categories.controller.js
@@ -21,4 +21,33 @@ router.post('/categories/save', (req, res) => {
     }
 });
 
+router.get('/admin/categories', (req, res) => {
+
+    Category.findAll().then(categories => {
+        res.render('admin/categories/index', {categories: categories});
+    });
+
+});
+
+router.post("/categories/delete", (req, res) => {
+    let id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+        }else{// NÃO FOR UM NÚMERO
+            res.redirect("/admin/categories");
+        }
+    }else{ // NULL
+        res.redirect("/admin/categories");
+    }
+});
+
+
+
 export default router;
