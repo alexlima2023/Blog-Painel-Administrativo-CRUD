69e7c06db78e9aa3c7072d03ad4ce7ed365eef5e
alexlima2023 <alexlimadev1997@gmail.com>
2023-06-21 13:27:56 -0300

    created edit form

--- controllers/categories/categories.controller.js
+++ controllers/categories/categories.controller.js
@@ -14,7 +14,7 @@ router.post('/categories/save', (req, res) => {
             title: title,
             slug: slugify(title) 
         }).then(() => {
-            res.redirect('/');
+            res.redirect('/admin/categories');
         });
     }else {
         res.redirect('/admin/categories/new');
@@ -40,13 +40,32 @@ router.post("/categories/delete", (req, res) => {
             }).then(() => {
                 res.redirect("/admin/categories");
             });
-        }else{// NÃO FOR UM NÚMERO
+        }else{// NÃO FOR UM NÚMERO 
             res.redirect("/admin/categories");
         }
     }else{ // NULL
         res.redirect("/admin/categories");
     }
 });
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(isNaN(id)) {
+        res.redirect('/admin/categories');
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined) {
+
+            res.render('admin/categories/edit', {category: category});
+
+        } else {
+            res.redirect('/admin/categories');
+        }
+    }).catch(erro => {  
+        res.redirect('/admin/categories');
+    });
+})
 
 
 
