f518f3a2708cc25ed1feebfc8e247606a0b19522
alexlima2023 <alexlimadev1997@gmail.com>
2023-06-22 12:52:41 -0300

    created select with categories

--- controllers/articles/articles.controller.js
+++ controllers/articles/articles.controller.js
@@ -1,12 +1,15 @@
 import express from "express";
 const router =  express.Router();
+import Category from "../../models/Category.js";
 
 router.get('/articles', (req, res) => {
     res.send("Rota de artigos"); 
 });
 
 router.get('/admin/articles/new', (req, res) => {
-    res.render('admin/articles/new');
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories})
+    })
 });
 
 export default router;
